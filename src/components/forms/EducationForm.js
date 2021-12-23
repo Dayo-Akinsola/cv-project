3c771a8855a28c68226dbc4a6dc63171c7347fb0
Dayo-Akinsola <dayoakinsola3@gmail.com>
2021-12-23 01:40:41 +0000

    implemented dynamic cv creation for education section

--- src/components/forms/EducationForm.js
+++ src/components/forms/EducationForm.js
@@ -2,22 +2,52 @@ import React, { Component} from 'react';
 import SectionHeading from './SectionHeading';
 
 class EducationForm extends Component {
-  constructor() {
-    super()
-  }
 
   render() {
+    const { educationFormState, handleInputChange, addForm } = this.props;
     return (
       <div className='section section--education'>
         <SectionHeading headingTextContent='Education' />
         <form className='form form--education'>
-          <input type='text' placeholder='University Name' className='form__input form__input--university-name' />
-          <input type='text' placeholder='Degree' className='form__input form__input--degree' />
-          <input type='text' placeholder='Department' className='form__input form__input--department' />
-          <input type='date' placeholder='Start Date' className='form__input form__input--start-date' />
-          <input type='date' placeholder='End Date' className='form__input form__input--end-date' />
-          <input type='text' placeholder='Relevant Modules' className='form__input form__input--relevant-modules' />
+          <input 
+            name='uniname' 
+            value={educationFormState.uniname} 
+            onChange={(event) => handleInputChange(event, educationFormState.id)} 
+            placeholder='University Name' 
+            className='form__input form__input--university-name' 
+          />
+          <input 
+            name='degree' 
+            value={educationFormState.degree} 
+            onChange={handleInputChange} 
+            placeholder='Degree' 
+            className='form__input form__input--degree' 
+          />
+          <input 
+            name='location' 
+            value={educationFormState.location} 
+            onChange={handleInputChange} 
+            placeholder='Location' 
+            className='form__input form__input--location' 
+          />
+          <input 
+            name='start' 
+            value={educationFormState.start} 
+            onChange={handleInputChange} 
+            placeholder='Start Year' 
+            className='form__input form__input--start-date' 
+          />
+          <input 
+            name='end' 
+            value={educationFormState.end} 
+            onChange={handleInputChange} 
+            placeholder='End Year' 
+            className='form__input form__input--end-date' 
+          />
         </form>
+        <div className="section add-btn-container">
+          <button onClick={addForm} className="section__add-education-btn btn">Add</button>
+        </div>
       </div>
 
     )
