2df83d728312e8cc38e67afe53a4764b303f5c8d
Dayo-Akinsola <dayoakinsola3@gmail.com>
2021-12-24 02:29:14 +0000

    experience form now dynamically renders cv template

--- src/components/forms/ExperienceForm.js
+++ src/components/forms/ExperienceForm.js
@@ -1,23 +1,80 @@
 import React, { Component} from 'react';
-import SectionHeading from './SectionHeading';
 
 class ExperienceForm extends Component {
-  constructor() {
-    super()
-  }
-
+ 
   render() {
+    const { form, tasks,  handleInputChange, handleTaskChange, deleteForm, deleteBtnClassName, addTaskEntry } = this.props;
+    const { title, company, location, start, end } = form;
+
     return (
       <div className='section section--experience'>
-        <SectionHeading headingTextContent='Experience' />
         <form className='form form--experience'>
-          <input type='text' placeholder='Job Title' className='form__input form__input--job-title' />
-          <input type='text' placeholder='Company Name' className='form__input form__input--company-name' />
-          <input type='text' placeholder='Location' className='form__input form__input--location' />
-          <input type='date' placeholder='Start Date' className='form__input form__input--start-date' />
-          <input type='date' placeholder='End Date' className='form__input form__input--end-date' />
-          <textarea placeholder='Responsiblity / Achivement' rows='4' className='form__input form__input--responsibility' />
+          <input 
+            name='title' 
+            type='text' 
+            value={title}
+            placeholder='Job Title' 
+            className='form__input form__input--job-title' 
+            onChange={(event) => handleInputChange(event, form.id)}
+          />
+          <input 
+            name='company' 
+            type='text' 
+            value={company}
+            placeholder='Company Name' 
+            className='form__input form__input--company-name' 
+            onChange={(event) => handleInputChange(event, form.id)}
+          />
+          <input 
+            name='location' 
+            type='text'
+            value={location} 
+            placeholder='Location' 
+            className='form__input form__input--location'
+            onChange={(event) => handleInputChange(event, form.id)} 
+          />
+          <input 
+            name='start' 
+            type='date' 
+            value={start}
+            placeholder='Start Date' 
+            className='form__input form__input--start-date' 
+            onChange={(event) => handleInputChange(event, form.id)}
+          />
+          <input 
+            name='end' 
+            type='date'
+            value={end} 
+            placeholder='End Date' 
+            className='form__input form__input--end-date' 
+            onChange={(event) => handleInputChange(event, form.id)}
+          />
+          <div className="responsibilities-container">
+            {tasks.map((task) => {
+               const button = task.addTaskBtn ? <button onClick={(event) => addTaskEntry(event, form.id)} className="add-responsibility-btn">&#43;</button>: <></>;
+               if (form.id === task.experienceFormId){
+                return(
+                  <div key={task.id} className="responsibility-container">
+                    <textarea 
+                      name='task' 
+                      value={task.text} 
+                      placeholder='Responsiblity / Achivement' 
+                      rows='2' 
+                      className='form__input form__input--responsibility' 
+                      onChange={(event) => handleTaskChange(event, form.id, task.id)}
+                    />
+                    {button}
+                  </div>
+                )
+               }
+               return null;
+              }
+            )}    
+          </div>
         </form>
+        <div className="section btn-container--delete">
+          <button onClick={() => deleteForm(form.id)} className={deleteBtnClassName}>Delete</button>
+        </div>
       </div>
     )
   }
