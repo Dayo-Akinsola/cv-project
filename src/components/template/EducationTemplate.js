da9077215f4a82c057d79bb330c4070098bf9ab6
Dayo-Akinsola <dayoakinsola3@gmail.com>
2021-12-24 02:27:52 +0000

    added function to format the date display

--- src/components/template/EducationTemplate.js
+++ src/components/template/EducationTemplate.js
@@ -3,18 +3,18 @@ import React, { Component } from 'react';
 class EducationTemplate extends Component {
 
   render() {
-    const { educationFormState } = this.props;
-    const { uniname, degree, location, start, end } = educationFormState;
+    const { formState, formatDate } = this.props;
+    const { uniname, degree, location, start, end } = formState;
+    const [ startDate, endDate ] = [start, end].map((date) => formatDate(date));
     return (
       <div className="template__container template__container--education">
-        <h3 className="template__heading template__heading--education"> Education </h3>
         <div className="template__content template__content--education">
           <div className="template__date-location template__date-location--education">
             <span className="template__location-name template_location-name--education">{location}</span>
             <div className="template__dates template__dates--education">
-              <span className="template__start-date template__start-date--education">{start}</span>
+              <span className="template__start-date template__start-date--education">{startDate}</span>
               <span className="dash"> - </span>
-              <span className="template__end-date template__end-date--education">{end}</span>
+              <span className="template__end-date template__end-date--education">{endDate}</span>
             </div>
           </div>
           <div className="template__university-details">
