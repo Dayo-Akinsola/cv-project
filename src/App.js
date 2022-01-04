6e3dc783a0ed58cf1d40bd7b88417e7f123ea6a4
Dayo Akinsola <dayoakinsola3@gmail.com>
2022-01-04 13:27:54 +0000

    changed to a functional component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from  'react';
+import React, { useState } from 'react';
 import PersonalForm from './components/forms/PersonalForm';
 import EducationForm from './components/forms/EducationForm';
 import ExperienceForm from './components/forms/ExperienceForm';
@@ -10,105 +10,87 @@ import SectionHeading from './components/SectionHeading';
 import PageHeader from './components/PageHeader';
 import { v4 as uuidv4 } from 'uuid';
 
+const App = () => {
+  const educationFormId = uuidv4();
+  const experienceFormId = uuidv4();
+  const taskID = uuidv4();
 
-class App extends Component {
-  constructor() {
-    super();
+  const [educationForms, setEducationForms] = useState(
+    [{
+      uniname: '',
+      degree: '',
+      location: '',
+      start: '',
+      end: '',
+      id: educationFormId  
+    }]
+  );
 
-    const educationFormId = uuidv4();
-    const experienceFormId = uuidv4();
-    const taskID = uuidv4();
-    this.state = {
-      educationForms: [{
-        uniname: '',
-        degree: '',
-        location: '',
-        start: '',
-        end: '',
-        id: educationFormId
-      }],
-      experienceForms: [{
-        title: '',
-        company: '',
-        location: '',
-        start: '',
-        end: '',
-        id: experienceFormId,
-      }],
-      tasks: [{
-        text: '',
-        id: taskID,
-        addTaskBtn: true,
-        experienceFormId,
-      }],
-      personalForm: {
-        first: '',
-        last: '',
-        email: '',
-        phone: '',
-        address: '',
-        title: '',
-        profile: '',
-      }
-    }
-    
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.addEducationForm = this.addEducationForm.bind(this);
-    this.deleteForm = this.deleteForm.bind(this);
-    this.formatDate = this.formatDate.bind(this);
-    this.addExperienceForm = this.addExperienceForm.bind(this);
-    this.handleTaskChange = this.handleTaskChange.bind(this);
-    this.addTaskEntry = this.addTaskEntry.bind(this);
-    this.deleteTaskEntry = this.deleteTaskEntry.bind(this);
-    this.resetForms = this.resetForms.bind(this);
-    this.autoFillForms = this.autoFillForms.bind(this);
-    this.printTemplate = this.printTemplate.bind(this);
-  }
+  const [experienceForms, setExperienceForms ] = useState([{
+    title: '',
+    company: '',
+    location: '',
+    start: '',
+    end: '',
+    id: experienceFormId,    
+  }]);
 
-  formatDate(date) {
+  const [tasks, setTasks] = useState([{
+    text: '',
+    id: taskID,
+    addTaskBtn: true,
+    experienceFormId,   
+  }]);
+
+  const [personalForm, setPersonalForm] = useState({
+    first: '',
+    last: '',
+    email: '',
+    phone: '',
+    address: '',
+    title: '',
+    profile: '',  
+  })
+
+  const formatDate = (date) => {
     if (date === '') return;
     const dateArray = date.split('-');
     const formattedDate = [dateArray[2], dateArray[1], dateArray[0]];
     return formattedDate.join('/');
   }
 
-  handleInputChange(event, id) {
+  const handleInputChange = (event, id) => {
     const { name, value } = event.target;
-    this.setState({
-      educationForms: this.state.educationForms.map((form) => {
-        if (form.id && form.id === id) {
-          form[name] = value;
-        }
-        return form;
-      }),
-      experienceForms: this.state.experienceForms.map((form) => {
-        if (form.id && form.id === id) {
-          form[name] = value;
-        }
-        return form;
-      }),
-      personalForm: {
-        ...this.state.personalForm,
-        [name]: value,
+    setEducationForms(educationForms.map(form => {
+      if (form.id && form.id === id) {
+        form[name] = value;
       }
-    });
-  }
-
-  handleTaskChange(event, formID, taskID) {
-    const { value } = event.target;
-
-
-    this.setState({
-      tasks: this.state.tasks.map((task) => {
-        if (task.experienceFormId === formID && task.id === taskID) {
-          task.text = value
-        }
-        return task;
-      })
+      return form
+    }));
+    setExperienceForms(experienceForms.map(form => {
+      if (form.id && form.id === id) {
+        form[name] = value;
+      } 
+      return form;
+    }))
+    setPersonalForm({
+      ...personalForm,
+      [name]: value,
     })
   }
 
-  addEducationForm() {
+  const handleTaskChange = (event, formID, taskID) => {
+    const { value } = event.target;
+
+    setTasks(tasks.map(task => {
+      if (task.experienceFormId === formID && task.id === taskID) {
+        task.text = value;
+      }
+      return task;
+    }))
+  }
+
+  const addEducationForm = () => {
 
     const newForm = {
       uniname: '',
@@ -119,12 +101,12 @@ class App extends Component {
       id: uuidv4(),    
     }
 
-    this.setState({
-      educationForms: this.state.educationForms.concat(newForm),
-    })
+    setEducationForms(
+      educationForms.concat(newForm)
+    )
   }
 
-  addExperienceForm() {
+  const addExperienceForm = () => {
 
     const newFormId = uuidv4();
     
@@ -137,6 +119,8 @@ class App extends Component {
       id: newFormId,
     }
 
+    setEducationForms(experienceForms.concat(newForm))
+
     const newTask = { 
       text: '',
       id: uuidv4(),
@@ -144,14 +128,11 @@ class App extends Component {
       experienceFormId: newFormId,  
     }
 
+    setTasks(tasks.concat(newTask))
 
-    this.setState({
-      experienceForms: this.state.experienceForms.concat(newForm),
-      tasks: this.state.tasks.concat(newTask),
-    })
   }
 
-  addTaskEntry(event, formID) {
+  const addTaskEntry = (event, formID) => {
     event.preventDefault();
     const newTaskEntry = {
       text: '',
@@ -160,45 +141,41 @@ class App extends Component {
       experienceFormId: formID,
     }
 
-    this.setState({
-      tasks: this.state.tasks.concat(newTaskEntry)
-    })
+    setTasks(tasks.concat(newTaskEntry));
   }
 
-  deleteTaskEntry(event, formID, taskID) {
+  const deleteTaskEntry = (event, formID, taskID) => {
     event.preventDefault();
-    this.setState({
-      tasks: this.state.tasks.filter((task) => {
-        if (task.experienceFormId === formID && task.id === taskID) {
-          return false;
-        }
-        return true;
-      })
-    })
+    setTasks(tasks.filter(task => {
+      if (task.experienceFormId === formID && task.id === taskID) {
+        return false;
+      }
+      return true;
+    }))
   }
 
-  deleteForm(id) {
-    this.setState({
-      educationForms: this.state.educationForms.filter((form) => form.id !== id),
-      experienceForms: this.state.experienceForms.filter((form) => form.id !== id),
-      tasks: this.state.tasks.filter((task) => task.experienceFormId !== id)
-    })
+  const deleteForm = (id) => {
+    setEducationForms(educationForms.filter((form) => form.id !== id));
+    setExperienceForms(experienceForms.filter((form) => form.id !== id));
+    setTasks(tasks.filter((task) => task.experienceFormId.id !== id));
   }
 
-  resetForms() {
+  const resetForms = () => {
     const educationFormId = uuidv4();
     const experienceFormId = uuidv4();
     const taskID = uuidv4();
-    this.setState({
-      educationForms: [{
+    setEducationForms(
+      [{
         uniname: '',
         degree: '',
         location: '',
         start: '',
         end: '',
         id: educationFormId
-      }],
-      experienceForms: [{
+      }],   
+    );
+    setExperienceForms(
+      [{
         title: '',
         company: '',
         location: '',
@@ -206,44 +183,48 @@ class App extends Component {
         end: '',
         id: experienceFormId,
       }],
-      tasks: [{
+    );
+    setTasks(
+      [{
         text: '',
         id: taskID,
         addTaskBtn: true,
         experienceFormId,
       }],
-      personalForm: {
-        first: '',
-        last: '',
-        email: '',
-        phone: '',
-        address: '',
-        title: '',
-        profile: '',
-        skills: [{
-          text: '',
-          id: uuidv4(),
-        }],
-      }
+    )
+    setPersonalForm({
+      first: '',
+      last: '',
+      email: '',
+      phone: '',
+      address: '',
+      title: '',
+      profile: '',
+      skills: [{
+        text: '',
+        id: uuidv4(),
+      }],
     })
   }
 
-  autoFillForms() {
+  const autoFillForms = () => {
     const educationFormId = uuidv4();
     const experienceFormIdOne = uuidv4();
     const experienceFormIdTwo = uuidv4();
     const experienceFormIdThree = uuidv4();
-    this.resetForms();
-    this.setState({
-      educationForms: [{
+    resetForms();
+    setEducationForms(
+      [{
         uniname: 'Imperial College London',
         degree: 'Computer Science',
         location: 'London, UK',
         start: '2016-09-20',
         end: '2019-06-25',
         id: educationFormId
-      }],
-      experienceForms: [
+      }]
+    );
+    setExperienceForms(
+      [
         {
           title: 'Data Engineer',
           company: 'National Health Service',
@@ -268,8 +249,10 @@ class App extends Component {
           end: '2019-05-19',
           id: experienceFormIdThree,
         }
-      ],
-      tasks: [
+      ]   
+    );
+    setTasks(
+      [
         {
           text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
           id: uuidv4(),
@@ -294,8 +277,10 @@ class App extends Component {
           addTaskBtn: true,
           experienceFormId: experienceFormIdThree,
         },              
-      ],
-      personalForm: {
+      ],  
+    );
+    setPersonalForm(
+      {
         first: 'John',
         last: 'Doe',
         email: 'johndoe@fakeemail.com',
@@ -304,95 +289,93 @@ class App extends Component {
         title: 'Data Engineer',
         profile: 'Nunc quis luctus lacus, quis mattis diam. Morbi varius scelerisque lacus, quis vestibulum nunc posuere bibendum. Donec sagittis enim justo. Sed placerat in nunc ut semper. Integer feugiat at mauris id sollicitudin.',
       }
-    })
+    )
   }
 
-  printTemplate() {
+  const printTemplate = () => {
     window.print();
   }
 
-  render() {
+
+  const deleteBtnClassNameEducation = 
+    educationForms.length === 1 ? 
+    'section__delete-education-btn btn--hidden' 
+    : 'section__delete-education-btn btn'
   
-    const deleteBtnClassNameEducation = 
-      this.state.educationForms.length === 1 ? 
+    const deleteBtnClassNameExperience = 
+      experienceForms.length === 1 ? 
       'section__delete-education-btn btn--hidden' 
       : 'section__delete-education-btn btn'
-    
-      const deleteBtnClassNameExperience = 
-        this.state.experienceForms.length === 1 ? 
-        'section__delete-education-btn btn--hidden' 
-        : 'section__delete-education-btn btn'
 
-    return(
-      <>
-      <PageHeader />
-      <div className="page-content">
-        <div className='forms'>
-          <SectionHeading sectionHeadingText='Personal' />
-          <PersonalForm formState={this.state.personalForm} handleInputChange={this.handleInputChange}  />
-          <SectionHeading sectionHeadingText='Education' />
-          {this.state.educationForms.map((form) => {
-            return (
-              <EducationForm 
-              key={form.id}
-              form={form} 
-              handleInputChange={this.handleInputChange}
-              addForm={this.addEducationForm}
-              deleteForm={this.deleteForm}
-              deleteBtnClassName={deleteBtnClassNameEducation}
-              />
-            )
-          })}
-           <div className="section btn-container--add">
-              <button onClick={this.addEducationForm} className="section__add-education-btn btn">Add</button>
-          </div>
-          <SectionHeading sectionHeadingText='Experience' />
-          {this.state.experienceForms.map((form) => {
-            return (
-              <ExperienceForm 
-                key={form.id}
-                form={form}
-                tasks={this.state.tasks}
-                handleInputChange={this.handleInputChange}
-                handleTaskChange={this.handleTaskChange}
-                addTaskEntry={this.addTaskEntry}
-                deleteTaskEntry={this.deleteTaskEntry}
-                deleteForm={this.deleteForm}
-                deleteBtnClassName={deleteBtnClassNameExperience}
-              />
-            )
-          })}
+  return(
+    <>
+    <PageHeader />
+    <div className="page-content">
+      <div className='forms'>
+        <SectionHeading sectionHeadingText='Personal' />
+        <PersonalForm formState={personalForm} handleInputChange={handleInputChange}  />
+        <SectionHeading sectionHeadingText='Education' />
+        {educationForms.map((form) => {
+          return (
+            <EducationForm 
+            key={form.id}
+            form={form} 
+            handleInputChange={handleInputChange}
+            addForm={addEducationForm}
+            deleteForm={deleteForm}
+            deleteBtnClassName={deleteBtnClassNameEducation}
+            />
+          )
+        })}
           <div className="section btn-container--add">
-            <button onClick={this.addExperienceForm} className="section__add-education-btn btn">Add</button>
-          </div>
-          <div className="form__control-btns">
-            <button onClick={this.printTemplate} type='button' className='submit-btn btn'>Save / Print</button>
-            <button onClick={this.autoFillForms} className="example-btn btn">Load Example</button>
-            <button onClick={this.resetForms} className="reset-btn btn">Reset</button>
-          </div>
-        </div> 
-        <div className="template">
-          <TemplateHeader formState={this.state.personalForm} />
-          <div className="template__sections">
-            <SectionHeading sectionHeadingText='Education' />
-            <hr />
-            {this.state.educationForms.map((form) => 
-              <EducationTemplate key={form.id} formState={form} formatDate={this.formatDate} />
-            )}
-            <SectionHeading sectionHeadingText='Experience' />
-            <hr />
-            <div className="template__section--experience">
-              {this.state.experienceForms.map((form) => 
-                <ExperienceTemplate key={form.id} formState={form} formatDate={this.formatDate} tasks={this.state.tasks} />
-              )}
-            </div>
-          </div>
-          <TemplateSidebar formState={this.state.personalForm} />
+            <button onClick={addEducationForm} className="section__add-education-btn btn">Add</button>
         </div>
+        <SectionHeading sectionHeadingText='Experience' />
+        {experienceForms.map((form) => {
+          return (
+            <ExperienceForm 
+              key={form.id}
+              form={form}
+              tasks={tasks}
+              handleInputChange={handleInputChange}
+              handleTaskChange={handleTaskChange}
+              addTaskEntry={addTaskEntry}
+              deleteTaskEntry={deleteTaskEntry}
+              deleteForm={deleteForm}
+              deleteBtnClassName={deleteBtnClassNameExperience}
+            />
+          )
+        })}
+        <div className="section btn-container--add">
+          <button onClick={addExperienceForm} className="section__add-education-btn btn">Add</button>
+        </div>
+        <div className="form__control-btns">
+          <button onClick={printTemplate} type='button' className='submit-btn btn'>Save / Print</button>
+          <button onClick={autoFillForms} className="example-btn btn">Load Example</button>
+          <button onClick={resetForms} className="reset-btn btn">Reset</button>
+        </div>
+      </div> 
+      <div className="template">
+        <TemplateHeader formState={personalForm} />
+        <div className="template__sections">
+          <SectionHeading sectionHeadingText='Education' />
+          <hr />
+          {educationForms.map((form) => 
+            <EducationTemplate key={form.id} formState={form} formatDate={formatDate} />
+          )}
+          <SectionHeading sectionHeadingText='Experience' />
+          <hr />
+          <div className="template__section--experience">
+            {experienceForms.map((form) => 
+              <ExperienceTemplate key={form.id} formState={form} formatDate={formatDate} tasks={tasks} />
+            )}
+          </div>
+        </div>
+        <TemplateSidebar formState={personalForm} />
       </div>
-      </>
-    )
-  }
+    </div>
+    </>
+  )
 }
 
 
--- src/components/forms/EducationForm.js
+++ src/components/forms/EducationForm.js
@@ -1,57 +1,54 @@
-import React, { Component} from 'react';
+import React from 'react';
 
-class EducationForm extends Component {
-
-  render() {
-    const { form, handleInputChange, deleteForm, deleteBtnClassName } = this.props;
-    return (
-      <div className='section section--education'>
-        <form className='form form--education'>
-          <input 
-            name='uniname' 
-            value={form.uniname} 
-            onChange={(event) => handleInputChange(event, form.id)} 
-            placeholder='University Name' 
-            className='form__input form__input--university-name' 
-          />
-          <input 
-            name='degree' 
-            value={form.degree} 
-            onChange={(event) => handleInputChange(event, form.id)} 
-            placeholder='Degree' 
-            className='form__input form__input--degree' 
-          />
-          <input 
-            name='location' 
-            value={form.location} 
-            onChange={(event) => handleInputChange(event, form.id)} 
-            placeholder='Location' 
-            className='form__input form__input--location' 
-          />
-          <input 
-            name='start' 
-            type='date'
-            value={form.start} 
-            onChange={(event) => handleInputChange(event, form.id)} 
-            placeholder='Start Year' 
-            className='form__input form__input--start-date' 
-          />
-          <input 
-            name='end'
-            type='date' 
-            value={form.end} 
-            onChange={(event) => handleInputChange(event, form.id)} 
-            placeholder='End Year' 
-            className='form__input form__input--end-date' 
-          />
-        </form>
-        <div className="section btn-container--delete">
-          <button onClick={() => deleteForm(form.id)} className={deleteBtnClassName}>Delete</button>
-        </div>
+const EducationForm = ({ form, handleInputChange, deleteForm, deleteBtnClassName }) => {
+  return (
+    <div className='section section--education'>
+      <form className='form form--education'>
+        <input 
+          name='uniname' 
+          value={form.uniname} 
+          onChange={(event) => handleInputChange(event, form.id)} 
+          placeholder='University Name' 
+          className='form__input form__input--university-name' 
+        />
+        <input 
+          name='degree' 
+          value={form.degree} 
+          onChange={(event) => handleInputChange(event, form.id)} 
+          placeholder='Degree' 
+          className='form__input form__input--degree' 
+        />
+        <input 
+          name='location' 
+          value={form.location} 
+          onChange={(event) => handleInputChange(event, form.id)} 
+          placeholder='Location' 
+          className='form__input form__input--location' 
+        />
+        <input 
+          name='start' 
+          type='date'
+          value={form.start} 
+          onChange={(event) => handleInputChange(event, form.id)} 
+          placeholder='Start Year' 
+          className='form__input form__input--start-date' 
+        />
+        <input 
+          name='end'
+          type='date' 
+          value={form.end} 
+          onChange={(event) => handleInputChange(event, form.id)} 
+          placeholder='End Year' 
+          className='form__input form__input--end-date' 
+        />
+      </form>
+      <div className="section btn-container--delete">
+        <button onClick={() => deleteForm(form.id)} className={deleteBtnClassName}>Delete</button>
       </div>
+    </div>
 
-    )
-  }
-}
+  )
+} 
+
 
 export default EducationForm;
